4e2fe2861464a52cc6ba7f36994b04066aedfce7
Dji-F <cross.miron@gmail.com>
2022-03-10 03:42:10 +0300

    create form, header, notes

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container pt-1">
+    <todo-header />
+    <todo-form @create="addNote" />
+    <todo-notes :notes="notes" />
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import TodoHeader from '@/components/TodoHeader'
+import TodoForm from '@/components/TodoForm'
+import TodoNotes from '@/components/TodoNotes'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data() {
+    return {
+      notes: [
+        {id: 1, title: 'Title', body: 'This is body text'}
+      ]
     }
-  }
+  },
+  methods: {
+    addNote(note) {
+      this.notes.push(note)
+    }
+  },
+  components: {TodoHeader, TodoForm, TodoNotes}
 }
+</script>
+
+<style>
+
 </style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+
+    <div class="form-control">
+      <label for="titleEnter">Title</label>
+      <input type="text" id="titleEnter" v-model.trim="note.title" />
+    </div>
+    <div class="form-control">
+      <label for="bodyEnter">Text</label>
+      <input type="text" id="bodyEnter" v-model.trim="note.body" />
+      <button class="btn" @click="createNote">Create</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['create'],
+  data() {
+    return {
+      note: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createNote() {
+      if (this.note.title && this.note.body) {
+        this.note.id = Date.now()
+        this.$emit('create', this.note)
+        this.note = {title: '', body: ''}
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+  margin-top: 10px;
+}
+</style>
