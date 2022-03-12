87b8d8b8fe9dafa3c47040d032f8cbb348d71506
Dji-F <cross.miron@gmail.com>
2022-03-12 07:33:50 +0300

    logic header finished

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container pt-1">
-    <todo-header />
-    <todo-form @create="addNote" />
-    <todo-notes v-if="notes.length" :notes="notes" @remove="removeNote"/>
+    <TodoHeader :active="active" @change="changeHandler"/>
+    <keep-alive>
+      <TodoForm v-if="active === 'notes'" @create="addNote" />
+    </keep-alive>
+    <TodoNotes
+        v-if="notes.length && active === 'notes'"
+        :notes="notes"
+        @remove="removeNote"
+        @save="saveNote"
+    />
+    <TodoSavedNotes v-else-if="active === 'saved'" :loading="loading" />
+    <TodoTrash v-else-if="active === 'trash'" />
   </div>
 </template>
 
@@ -10,24 +19,46 @@
 import TodoHeader from '@/components/TodoHeader'
 import TodoForm from '@/components/TodoForm'
 import TodoNotes from '@/components/TodoNotes'
+import TodoSavedNotes from '@/components/TodoSavedNotes'
+import TodoTrash from '@/components/TodoTrash'
 
 export default {
   data() {
     return {
-      notes: [
-        {id: 1, title: 'Title', body: 'This is body text'}
-      ]
+      notes: [],
+      savedNotes: [],
+      active: 'notes',
+      loading: false
     }
   },
   methods: {
     addNote(note) {
       this.notes.push(note)
     },
-    removeNote(idx) {
-      this.notes.splice(idx, 1)
+    removeNote(id) {
+      this.notes = this.notes.filter(note => note.id !== id)
+    },
+    async saveNote(note) {
+      try {
+        const response = await fetch('https://todo-list-vue-78664-default-rtdb.firebaseio.com/notes.json', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify(note)
+        })
+        const firebaseData = await response.json()
+        this.removeNote(note.id)
+      } catch (e) {
+        this.loading = false
+        console.error(e.message)
+      }
+    },
+    changeHandler(btn) {
+      this.active = btn
     }
   },
-  components: {TodoHeader, TodoForm, TodoNotes}
+  components: {TodoHeader, TodoForm, TodoNotes, TodoSavedNotes, TodoTrash}
 }
 </script>
 
---
+++ src/components/TodoTrash.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="card">
+    <h3>This is trash</h3>
+  </div>
+</template>
